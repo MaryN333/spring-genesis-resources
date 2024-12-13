3f1a1022d302f01df6ee9ba08f2ee3f7a811ea06
MaryN333 <sidyakina@gmail.com>
2024-12-13 15:07:45 +0100

    Initial commit

---
+++ src/main/java/cz/wz/marysidy/spring_genesis_resources/SpringGenesisResourcesApplication.java
@@ -0,0 +1,13 @@
+package cz.wz.marysidy.spring_genesis_resources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGenesisResourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGenesisResourcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/wz/marysidy/spring_genesis_resources/SpringGenesisResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.wz.marysidy.spring_genesis_resources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGenesisResourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
